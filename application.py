3a9dcf3087c14bce230576180073768f15321f9d
natalie-poulson <natalieplsn@gmail.com>
2020-04-24 19:35:25 -0700

    added sessions

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 # import datetime
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -48,19 +49,35 @@ app = Flask(__name__)
 # def index():
 #     return render_template('index.html')
 
-@app.route('/more')
-def more():
-    return render_template('more.html')
+# @app.route('/more')
+# def more():
+#     return render_template('more.html')
 
-# Forms
-@app.route('/')
+# # Forms
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/hello', methods=['GET', 'POST'])
+# def hello():
+#     if request.method == 'GET':
+#         return "Please submit the form instead."
+#     else:
+#         name = request.form.get('name')
+#         return render_template('hello.html', name=name)
+
+# Notes App
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = 'filesystem'
+Session(app)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if session.get('notes') is None:
+        session['notes'] = []
+    
+    if request.method == 'POST':
+        note = request.form.get('note')
+        session['notes'].append(note)
 
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
-    if request.method == 'GET':
-        return "Please submit the form instead."
-    else:
-        name = request.form.get('name')
-        return render_template('hello.html', name=name)
+    return render_template('index.html', notes=session['notes'])
