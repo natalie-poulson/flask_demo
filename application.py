570edf669740add555c993b7486acd8a01873d31
natalie-poulson <natalieplsn@gmail.com>
2020-04-24 17:41:19 -0700

    introducing templates

--- application.py
+++ application.py
@@ -1,15 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#  Intial Practice
+# @app.route('/')
+# def index():
+#     return 'Hello, world!'
+
+# @app.route('/nat')
+# def nat():
+#     return 'Hello, nat!'
+
+# @app.route('/<string:name>')
+# def hello(name):
+#     name = name.capitalize()
+#     return f'<h1>Hello, {name}!<h2>'
+
+# Working with HTML files
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    headline = 'Hello!'
+    return render_template('index.html', headline=headline)
 
-@app.route('/nat')
-def nat():
-    return 'Hello, nat!'
-
-@app.route('/<string:name>')
-def hello(name):
-    return f'Hello, {name}!'
+@app.route('/bye')
+def bye():
+    headline = 'See ya'
+    return render_template('index.html', headline=headline)
